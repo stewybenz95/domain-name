15f23daddeb29d67e3dcbd86ae27e7a6ccf91521
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2024-05-18 15:19:09 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
+const domain = () => {
+  let first = ["apple", "banana", "candy"];
+  let second = ["butter", "pecan", "alcohol"];
+  let third = [".com", ".org", ".net", ".gov"];
 
-domain = () => {
-  let first = ["apple", "banana", "candy"] 
-  let second = ["butter", "pecan", "alcohol"]
-  let third = [".com", ".org", ".net", ".gov"]
-
-    for (let i = 0; i < first.length; i++)
+  for (let i = 0; i < first.length; i++)
     for (let a = 0; a < second.length; a++)
-    for (let b = 0; b < third.length; b++)
-    console.log (first[i] + second[a] + third[b])
-    
-}
-console.log(domain())
+      for (let b = 0; b < third.length; b++)
+        console.log(first[i] + second[a] + third[b]);
+};
+console.log(domain());
